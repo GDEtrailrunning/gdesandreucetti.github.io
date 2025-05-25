a1794bbcd43dc3db4475c935306a0fd7dfd6ee29
GDEtrailrunning <gdeconcienciacorporal@gmail.com>
2025-05-25 20:43:59 -0300

    Commit weeks 19 & 20

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ document.addEventListener("DOMContentLoaded", function () {
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 17',
-    data: [12, 15, 12, 14, 16, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 19',
+    data: [11, 15, 16, 19, 15, 25], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4', 'Día 5', 'Día 6'], // Etiquetas de los días
   datasets: [{
-    label: 'Km recorridos en Semana 18',
-    data: [14, 23, 14, 5, 14, 30], // Datos de los km recorridos en cada día    borderColor: 'rgba(153, 102, 255, 1)',
+    label: 'Km recorridos en Semana 20',
+    data: [14, 13, 19, 13, 13, 25], // Datos de los km recorridos en cada día    borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
   }]
